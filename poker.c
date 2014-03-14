60983b4a1d3df82368f319d4248d420b83909a74
Giancarlo <giancarlokc@gmail.com>
2014-03-13 19:56:34 -0700

    Added license

--- poker.c
+++ poker.c
@@ -1,3 +1,20 @@
+// Copyright 2012 Giancarlo Klemm Camilo, Renan Greca
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// ********************************************************************* //
+
 // Project 3 of Computer Networks. Students: Giancarlo Klemm Camilo, Renan Greca
 // UFPR 2012/2
 
